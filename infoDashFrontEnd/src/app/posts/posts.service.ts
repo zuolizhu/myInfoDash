7829a7323f9e7a442aff9a05aa92ed53e8ce67fb
Zuoli Zhu <zuolizhu5960x@gmail.com>
2019-02-06 19:23:06 -0500

    post handled

--- infoDashFrontEnd/src/app/posts/posts.service.ts
+++ infoDashFrontEnd/src/app/posts/posts.service.ts
@@ -26,7 +26,11 @@ export class PostsService {
     }
 
     addPost(post: Post) {
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
+        this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+        .subscribe((resData) => {
+            console.log(resData.message);
+            this.posts.push(post);
+            this.postsUpdated.next([...this.posts]);
+        });
     }
 }
